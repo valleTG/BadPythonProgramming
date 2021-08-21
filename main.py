ed3974033370d9bd4bdb78c79d17ba1c1d5c352a
valleTG <valle.rudhage@gmail.com>
2021-08-21 16:11:20 +0100

    For Loops & Nested Loops

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-# While loops
+# Imports
 
-name = None
+import time
 
-while not name:
-    name = input("Enter your name: ")
+# While Loops
 
-print("Hello " + name)
+# name = None
+
+# while not name:
+#     name = input("Enter your name: ")
+
+# print("Hello " + name)
+
+# For Loops
+
+# for i in range(10):
+#     print(i + 1)
+
+# for i in range(50, 100 + 1, 2):
+#     print(i)
+
+# for i in "Valle TG":
+#     print(i)
+
+# for seconds in range(10, 0, -1):
+#     print(seconds)
+#     time.sleep(1)
+# print("Happy New Year!")
+
+# Nested Loops
+
+# rows = int(input("How many rows?: "))
+# columns = int(input("How many columns?: "))
+# symbol = input("Enter a symbol to use: ")
+
+# for i in range(rows):
+#     for j in range(columns):
+#         print(symbol, end="")
